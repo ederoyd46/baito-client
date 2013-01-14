164af754fbbce8933068ddaa5a8be5d58cc2b1e9
Matthew Brown <matt@ederoyd.co.uk>
2013-01-14 11:50:38 +0000

    updated with callback function

--- src/baito.c
+++ src/baito.c
@@ -4,115 +4,77 @@
 
 #include <curl/curl.h>
 
-/* Auxiliary function that waits on the socket. */
-static int wait_on_socket(curl_socket_t sockfd, int for_recv, long timeout_ms)
+struct MemoryStruct {
+  char *memory;
+  size_t size;
+};
+ 
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-  struct timeval tv;
-  fd_set infd, outfd, errfd;
-  int res;
-
-  tv.tv_sec = timeout_ms / 1000;
-  tv.tv_usec= (timeout_ms % 1000) * 1000;
-
-  FD_ZERO(&infd);
-  FD_ZERO(&outfd);
-  FD_ZERO(&errfd);
-
-  FD_SET(sockfd, &errfd); /* always check for error */
-
-  if(for_recv)
-  {
-    FD_SET(sockfd, &infd);
+  size_t realsize = size * nmemb;
+  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+ 
+  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+  if (mem->memory == NULL) {
+    /* out of memory! */ 
+    printf("not enough memory (realloc returned NULL)\n");
+    exit(EXIT_FAILURE);
   }
-  else
-  {
-    FD_SET(sockfd, &outfd);
-  }
+ 
+  memcpy(&(mem->memory[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+ 
+  return realsize;
+}
 
-  /* select() returns the number of signalled sockets or -1 */
-  res = select(sockfd + 1, &infd, &outfd, &errfd, &tv);
-  return res;
+
+static char* get_data(char *url) {
+  CURL *curl_handle;
+  CURLcode res;
+  struct MemoryStruct chunk;
+  chunk.memory = malloc(1);
+  chunk.size = 0;
+ 
+  curl_global_init(CURL_GLOBAL_ALL);
+  curl_handle = curl_easy_init();
+  
+  if(curl_handle) {
+    curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+    res = curl_easy_perform(curl_handle);
+    if(res != CURLE_OK) fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    
+    curl_easy_cleanup(curl_handle);
+  }
+ 
+  printf("%lu bytes retrieved\n", (long)chunk.size);
+  curl_global_cleanup();
+ 
+  return chunk.memory;
 }
 
 int jobs_search(char *searchTerm) {
-  CURL *curl;
-  CURLcode res;
-  curl_socket_t sockfd;
-  long sockextr;
-  size_t iolen;
-  curl_off_t nread;
+  char *api = "https://baito.co.uk/api/search?searchTerm=%s";
+  char *apiUrl = malloc(strlen(api) + strlen(searchTerm) +1);
+  sprintf(apiUrl, api, searchTerm);
+  char *response = get_data(apiUrl);
+  puts(response);
   
-  char *preApi = "/api/search?searchTerm=%s";
-  char *api = malloc(25 + strlen(searchTerm) + 1); //Include +1 for terminating null??
-  sprintf(api, preApi, searchTerm);
-
-  char *preRequest = "GET %s HTTP/1.1\r\nHost: baito.co.uk\r\n\r\n;";
-  char *request = malloc(strlen(preRequest) + strlen(api) +1);
-  sprintf(request, preRequest, api);
-  
-  
-  curl_global_init(CURL_GLOBAL_DEFAULT);
-  curl = curl_easy_init();
-  
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://baito.co.uk");
-    curl_easy_setopt(curl, CURLOPT_CONNECT_ONLY, 1L);
-    // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); -- Uncomment if the certificate is incorrect
-    // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-  
-    res = curl_easy_perform(curl);
-    if(res != CURLE_OK) fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-    
-    res = curl_easy_getinfo(curl, CURLINFO_LASTSOCKET, &sockextr);
-    if(res != CURLE_OK) fprintf(stderr, "curl_easy_getinfo() failed: %s\n", curl_easy_strerror(res));
-    
-    sockfd = sockextr;
-    /* wait for the socket to become ready for sending */
-    if(!wait_on_socket(sockfd, 0, 60000L))
-    {
-      printf("Error: timeout.\n");
-      return 1;
-    }
-  
-    puts("Sending request.");
-  
-    res = curl_easy_send(curl, request, strlen(request), &iolen);
-    if(res != CURLE_OK) fprintf(stderr, "curl_easy_send() failed: %s\n", curl_easy_strerror(res));
-  
-    puts("Reading response.");
-  
-    /* read the response */
-    char *response = malloc(1024);
-    for(;;)
-    {
-      char buf[1024];
-  
-      wait_on_socket(sockfd, 1, 60000L);
-      res = curl_easy_recv(curl, buf, 1024, &iolen);
-  
-      if(CURLE_OK != res) 
-        break;
-      
-      nread = (curl_off_t)iolen;
-      
-      char *existing = malloc(strlen(response));
-      strcpy(existing, response);
-      response = malloc(strlen(response) + strlen(buf));
-      strcpy(response, existing);
-      strcat(response, buf);
-  
-      printf("Received %" CURL_FORMAT_CURL_OFF_T " bytes.\n", nread);
-    }
-    
-    puts(response);
-    
-    curl_easy_cleanup(curl);
+  if (response) {
+    free(response);
   }
-   
-  curl_global_cleanup();
-   
+  // if(chunk.memory) {
+  //   free(chunk.memory);
+  // }
+  //   
+  //  
+  /* we're done with libcurl, so clean it up */ 
+  
+  
+  
   return 0;
 }
-
-
 
