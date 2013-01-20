5b512998112fe5d35cec2572726b374825a9c788
Matthew Brown <matt@ederoyd.co.uk>
2013-01-20 12:31:46 +0000

    bit of padding

--- src/main.c
+++ src/main.c
@@ -27,10 +27,8 @@ int main(int argc, char * argv[])
   printf("Longitude: %G\n", res.longitude);
   puts("-------------------------------------------------");
 
-  // printf("title: %s\n", res.firstResult.title);
-  // printf("test2: %s\n", res.test[0].text);
   for (i=0; i<res.count; i++) {
-    printf("distance: %-5G uuid: %-40s company: %-40s title: %-40s\n", res.results[i].distance, res.results[i].uuid, res.results[i].company, res.results[i].title);
+    printf("distance: %-5G| uuid: %-40s| company: %-40s| title: %-40s\n", res.results[i].distance, res.results[i].uuid, res.results[i].company, res.results[i].title);
   }
   puts("-------------------------------------------------");
   
