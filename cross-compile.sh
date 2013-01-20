ba03ecf18ee79cabc6410b96831d12ccb8a6cb05
Matthew Brown <matt@ederoyd.co.uk>
2013-01-20 18:24:42 +0000

    added common library for development

--- cross-compile.sh
+++ cross-compile.sh
@@ -16,4 +16,16 @@ if [ "$uname" == 'Darwin' ]; then
 
   setenv_i386
   make CROSS_COMPILE=i386
+  
+  BUILD=./build
+  COMMON_LIB_LIB=$BUILD/common-libbaito/lib
+  COMMON_INCLUDE=$BUILD/common-libbaito/include
+  
+  mkdir -p $COMMON_LIB_LIB $COMMON_INCLUDE
+  cp -r $BUILD/libbaito/include/* $COMMON_INCLUDE
+  
+  lipo -create -output $COMMON_LIB_LIB/libbaito.a $BUILD/arm7s-libbaito/lib/libbaito.a \
+    $BUILD/arm7-libbaito/lib/libbaito.a $BUILD/i386-libbaito/lib/libbaito.a $BUILD/libbaito/lib/libbaito.a
+  
+  
 fi
