8f1786c9859f05097f2ca33a98373b01923c7a18
Matthew Brown <matt@ederoyd.co.uk>
2013-02-01 17:51:22 +0000

    added re-search on search

--- ios/test2/testMasterViewController.m
+++ ios/test2/testMasterViewController.m
@@ -15,6 +15,8 @@
 NSMutableArray *_objects;
 CLLocationManager *_locationManager;
 CLLocation *_currentLocation;
+SearchResultsResponse lastSearchResultResponse;
+BOOL searchRunning = NO;
 
 - (void)awakeFromNib
 {
@@ -59,7 +61,7 @@ CLLocation *_currentLocation;
 
 -(void)currentLocationSearchButtonClicked:(UIBarButtonItem *)currentSearchButton
 {
-  [self runSearch:YES];
+  [self runSearch:YES isSearchMore:NO];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
@@ -67,58 +69,60 @@ CLLocation *_currentLocation;
   for (CLLocation *location in locations) {
     _currentLocation = location;
     self.navigationItem.rightBarButtonItem.enabled = YES;
-//    NSLog(@"async lat: %f lon: %f", location.coordinate.latitude, location.coordinate.longitude);
   }
 }
-
 
 -(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
 {
   [searchBar resignFirstResponder];
-  [self runSearch:NO];
+  [self runSearch:NO isSearchMore:NO];
 }
 
-
-- (void) runSearch:(BOOL)isByLocation
+- (void) runSearch:(BOOL)isByLocation isSearchMore:(BOOL)searchMore
 {
-  UIApplication *application = [UIApplication sharedApplication];
-  application.networkActivityIndicatorVisible = YES;
-  
-  if (!_objects) {
-    _objects = [[NSMutableArray alloc] init];
-  }
-  
-  //  __block NSMutableArray *newresults = [[NSMutableArray alloc] init];
-  [_objects removeAllObjects];
-  
-  const char *term = [_searchTerm.text cStringUsingEncoding:NSUTF8StringEncoding];
-  const CLLocation *location = _currentLocation;
-  
-  dispatch_queue_t searchQueue = dispatch_queue_create("Baito Search Queue", NULL);
-  
-  dispatch_async(searchQueue, ^{
-    SearchResultsResponse res;
-    if (isByLocation) {
-      res = jobs_direct_search(location.coordinate.latitude, location.coordinate.longitude);
-    } else {
-      res = jobs_search((char *)term);
-    }
+  if (!searchRunning) {
+    searchRunning = YES;
+    UIApplication *application = [UIApplication sharedApplication];
+    application.networkActivityIndicatorVisible = YES;
+
     
-    int i;
-    for (i = 0; i < res.count; i++) {
-      NSString *uuid = [NSString stringWithCString:res.results[i].uuid encoding:NSUTF8StringEncoding];
-      NSString *title = [NSString stringWithCString:res.results[i].title encoding:NSUTF8StringEncoding];
-      NSDictionary *entry = @{@"uuid": uuid, @"title": title};
-      [_objects addObject:entry];
-    }
+    __block NSMutableArray *results = [[NSMutableArray alloc] init];
     
-    clear_job_search(res);
+    const char *term = [_searchTerm.text cStringUsingEncoding:NSUTF8StringEncoding];
+    const CLLocation *location = _currentLocation;
     
-    dispatch_async(dispatch_get_main_queue(), ^{
-      [self.tableView reloadData];
-      application.networkActivityIndicatorVisible = NO;
+    dispatch_queue_t searchQueue = dispatch_queue_create("Baito Search Queue", NULL);
+    
+    dispatch_async(searchQueue, ^{
+      SearchResultsResponse res;
+      if (searchMore) {
+        res = jobs_search_for_more(lastSearchResultResponse);
+      } else {
+        clear_job_search(lastSearchResultResponse);
+        if (isByLocation) {
+          res = jobs_direct_search(location.coordinate.latitude, location.coordinate.longitude);
+        } else {
+          res = jobs_search((char *)term);
+        }
+      }
+
+      lastSearchResultResponse = res;
+      int i;
+      for (i = 0; i < res.count; i++) {
+        NSString *uuid = [NSString stringWithCString:res.results[i].uuid encoding:NSUTF8StringEncoding];
+        NSString *title = [NSString stringWithCString:res.results[i].title encoding:NSUTF8StringEncoding];
+        NSDictionary *entry = @{@"uuid": uuid, @"title": title};
+        [results addObject:entry];
+      }
+
+      dispatch_async(dispatch_get_main_queue(), ^{
+        _objects = results;
+        [self.tableView reloadData];
+        application.networkActivityIndicatorVisible = NO;
+        searchRunning = NO;
+      });
     });
-  });
+  }
 }
 
 
@@ -146,6 +150,22 @@ CLLocation *_currentLocation;
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
   return NO;
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  float row = indexPath.row+1;
+  float total = _objects.count;
+  
+  float percent = (row/total)*100;
+  
+  if (percent >= 95) {
+    [self runSearch:NO isSearchMore:YES];
+//    NSLog(@"trigger search");
+  }
+  
+//  NSLog(@"showing cell %i of %i = %f", indexPath.row, _objects.count, percent);
+  
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
